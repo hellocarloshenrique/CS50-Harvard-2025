31d8b7176768ffe0c10f290394c27cdb674d08a7
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2025-07-03 06:57:24 -0300

    primeiro programa em c

---
+++ lecture1/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Inclui a biblioteca padrao de entrada e saida
+
+int main() {
+    printf("Ola, Carlos! Meu primeiro programa em C no Linux Mint!\n");
+    return 0; // Indica que o programa terminou com sucesso
+}
