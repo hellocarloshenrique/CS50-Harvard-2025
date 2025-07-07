6fa939626c0256364a3b9534d8b81c41dc14669c
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2025-07-07 06:25:57 -0300

    meow five times

---
+++ meaw.c
@@ -0,0 +1,27 @@
+#include <stdio.h> // Inclui a biblioteca padrao de entrada e saida
+/*
+#include <cs50.h>
+int main() {
+    printf("Ola, Carlos! Meu primeiro programa em C no Linux Mint!\n");
+    return 0; // Indica que o programa terminou com sucesso
+}
+void meow(int n);
+
+int main(void)
+{
+    int n = get_int("Number: ");
+    meow(n);
+}
+*/
+
+
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        printf("meow\n");
+    }
+    
+}
