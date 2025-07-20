84d505fe91ef824a4946bd782970203f531d726a
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2025-07-20 17:34:27 -0300

    usando funções e for
    aqui estamos criando blocos iguais do cenário do Mario Bros para entender o conceito de função e chamada de função e declaração de variáveis e usando o laço for.

---
+++ lecture1/mario.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void print_row(int n);
+
+int main (void)
+{   
+    int n = 3;
+    for (int row = 0; row < n; row++)
+    {
+        print_row(n);
+    }
+}
+
+void print_row(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+    
+}
